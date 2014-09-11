9185d411435c8211b0a04720e946149c8b8ef80d
Shad Storhaug <shad@shadstorhaug.com>
2014-09-11 17:29:56 +0700

    Created demo project showing how to wrap a IAclModule within a ISiteMapNodeVisibilityProvider so the VisibilityAffectsDescendants behavior can be used with AclModules.

---
+++ MvcSiteMapProvider_355/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSiteMapProvider_355.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult AboutMe()
+        {
+            ViewBag.Message = "About Me.";
+
+            return View();
+        }
+
+        public ActionResult AboutYou()
+        {
+            ViewBag.Message = "About You.";
+
+            return View();
+        }
+
+        
+        public ActionResult AboutUs()
+        {
+            ViewBag.Message = "About Us.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcSiteMapProvider_355/VisibilityProviders/AclModuleVisibilityProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using MvcSiteMapProvider;
+using MvcSiteMapProvider.Security;
+using MvcSiteMapProvider.Web.Mvc;
+using MvcSiteMapProvider.Web.Mvc.Filters;
+
+namespace MvcSiteMapProvider_355.VisibilityProviders
+{
+    public class AclModuleVisibilityProvider
+        : SiteMapNodeVisibilityProviderBase
+    {
+        public AclModuleVisibilityProvider(
+            IAclModule aclModule
+            )
+        {
+            if (aclModule == null)
+                throw new ArgumentNullException("aclModule");
+
+            this.aclModule = aclModule;
+        }
+        private readonly IAclModule aclModule;
+
+        public override bool IsVisible(ISiteMapNode node, IDictionary<string, object> sourceMetadata)
+        {
+            return this.aclModule.IsAccessibleToUser(node.SiteMap, node);
+        }
+    }
+}
